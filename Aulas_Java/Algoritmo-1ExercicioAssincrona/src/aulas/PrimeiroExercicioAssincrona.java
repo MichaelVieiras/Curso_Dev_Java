220811a974e9e5b19d18aa388f8553352c07b933
Michael Vieira <devmchvds@gmail.com>
2022-07-27 18:46:52 -0300

    Update PrimeiroExercicioAssincrona.java

--- Aulas_Java/Algoritmo-1ExercicioAssincrona/src/aulas/PrimeiroExercicioAssincrona.java
+++ Aulas_Java/Algoritmo-1ExercicioAssincrona/src/aulas/PrimeiroExercicioAssincrona.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class PrimeiroExercicioAssincrona {
 
-	@SuppressWarnings("unlikely-arg-type")
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in).useLocale(Locale.US);
