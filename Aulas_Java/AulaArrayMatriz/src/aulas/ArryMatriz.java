f896437987f9020fc4dd1eb3351cece0def49ba9
Michael Vieira <devmchvds@gmail.com>
2022-07-29 17:19:29 -0300

    Update ArryMatriz.java

--- Aulas_Java/AulaArrayMatriz/src/aulas/ArryMatriz.java
+++ Aulas_Java/AulaArrayMatriz/src/aulas/ArryMatriz.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class ArryMatriz {
 
+	private static final int String = 0;
+
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
@@ -15,7 +17,7 @@ public class ArryMatriz {
 		System.out.println(numero[4]);//resultado vai ser zero, pois temos somente a quantidade de posições
 		System.out.println(numero.length);// obtem o tamanho do array
 		*/
-		System.out.println("Entre com a quantidade: ");
+		/*System.out.println("Entre com a quantidade: ");
 		int qtd = scan.nextInt();
 		
 		int[] passadoTeclado = new int[qtd];
@@ -30,7 +32,7 @@ public class ArryMatriz {
 		for(int numero : numeros) {
 			System.out.println(numero);
 		}
-		/*System.out.println("Entre com a quantidade de nomes: ");
+		System.out.println("Entre com a quantidade de nomes: ");
 		int qtd = scan.nextInt();
 		
 		String[] nomes = new String[qtd];
@@ -49,6 +51,38 @@ public class ArryMatriz {
 		for(String nome : nomes) {
 			System.out.println(nome);
 		}*/
+		
+		String[][] numerosArray = new String[3][3];
+		int contador = 10;
+		
+		for(int i = 0; i < numerosArray.length; i++) {
+			
+			for(int j = 0; j < numerosArray.length; j++) {
+				numerosArray[i][j] = Integer.toString(contador);
+				if(numerosArray[i] != numerosArray[j]) {
+					numerosArray[i][j] = " ";										
+				}
+				System.out.print(numerosArray[i][j] + "\t");
+				contador++;
+			}
+			System.out.println("\n");
+		}
+
+		
+		/*if(numerosArray[i] == numerosArray[j]) {
+			System.out.print(numerosArray[i][j] + "\t");
+			contador++;
+		}
+		numerosArray[0][0] = Integer.toString(contador);;
+		numerosArray[0][1] = " ";
+		numerosArray[0][2] = " ";
+		numerosArray[1][0] = " ";
+		numerosArray[1][1] = Integer.toString(contador);
+		numerosArray[1][2] = " ";
+		numerosArray[2][0] = " ";
+		numerosArray[2][1] = " ";
+		numerosArray[2][2] = Integer.toString(contador);*/
+		
 			
 		scan.close();
 	}
