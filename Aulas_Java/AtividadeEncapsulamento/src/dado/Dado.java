7d713f43118049163ca158241c6757668a5baf25
Michael Vieira <devmchvds@gmail.com>
2022-08-08 17:44:23 -0300

    Update Dado.java

--- Aulas_Java/AtividadeEncapsulamento/src/dado/Dado.java
+++ Aulas_Java/AtividadeEncapsulamento/src/dado/Dado.java
@@ -3,25 +3,23 @@ package dado;
 import java.util.Scanner;
 
 public class Dado {
+	public int dado;
 	
-	public char dado;
-	
-	public Dado() {
-		
+	public void Dado(int dado) {
 		Scanner scan = new Scanner(System.in);
-		char opcao;
 		
-		System.out.println("Deseja lançar o dado (s/n)?");
-		opcao = scan.next().charAt(0);
-		while (opcao == 's') {
+		char opcao2 = (char) dado;
+		
+		while (opcao2 == 's') {
 			System.out.println("Lançamento do DADO.....................");
-			int dado = (int) (Math.random() * 6 + 1);
-			System.out.println("Face: " + dado);
-			System.out.println("Deseja lançar o dado novamente (s/n)?");
-			opcao = scan.next().charAt(0);		
+			int dado2 = (int) (Math.random() * 6 + 1);
+			System.out.println("Face: " + dado2);
+			System.out.println("Deseja lançar o dado novamente | 1 - Sim | 2 - Não |?");
+			opcao2 = scan.next().charAt(0);	
 		}
 		
-		scan.close();
+		System.out.println("Encerrado");
 		
+		scan.close();
 	}
 }
