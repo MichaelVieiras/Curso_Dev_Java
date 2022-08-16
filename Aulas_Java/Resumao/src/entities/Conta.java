da09e0d138bc644ca6a93cdb451fda58da2fff8c
Michael Vieira <devmchvds@gmail.com>
2022-08-16 00:22:16 -0300

    Update Conta.java

--- Aulas_Java/Resumao/src/entities/Conta.java
+++ Aulas_Java/Resumao/src/entities/Conta.java
@@ -5,56 +5,61 @@ public class Conta {
 	private int numeroConta;
 	private int agencia;
 	private double saldo;
-	private String titularDaConta;
+	private Clientes titularDaConta;
 	
 	
 	public Conta() {
 	}
 
-
 	public int getNumeroConta() {
 		return numeroConta;
 	}
-
 
 	public void setNumeroConta(int numeroConta) {
 		this.numeroConta = numeroConta;
 	}
 
-
 	public int getAgencia() {
 		return agencia;
 	}
-
 
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
 
-
-	public String getTitularDaConta() {
+	public Clientes getTitularDaConta() {
 		return titularDaConta;
 	}
 
-
-	public void setTitularDaConta(String titularDaConta) {
+	public void setTitularDaConta(Clientes titularDaConta) {
 		this.titularDaConta = titularDaConta;
 	}
-
 
 	public double getSaldo() {
 		return saldo;
 	}
-	public void Depositar (double valor) {
+
+	public void Depositar(double valor) {
 		this.saldo += valor;
 	}
-	public boolean Sacar (double valor) {
-		if(valor > this.saldo) {
-			return false;
-		}else {
+
+	public boolean Sacar(double valor) throws ContaException {
+		if (valor <= this.saldo) {
 			this.saldo -= valor;
 			return true;
+		} else {
+			throw new ContaException("Saldo indisponivel!");
+			//return false;
 		}
-		
+	}
+
+	public boolean Transferir(double valor, Conta destino) throws ContaException {
+		if (valor <= saldo) {
+			this.saldo -= valor;
+			destino.saldo += valor;
+			return true;
+		}
+		throw new ContaException("Saldo indisponivel!");
+		//return false;
 	}
 }
