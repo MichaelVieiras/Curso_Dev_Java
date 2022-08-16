494b07d5885a70c9f3f855cc748ae4330961a29f
Michael Vieira <devmchvds@gmail.com>
2022-08-16 14:28:04 -0300

    Update Resumão

---
+++ Aulas_Java/Resumao/src/entities/ContaException.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class ContaException extends Exception{
+	private static final long serialVersionUID = 1L;
+	
+	public ContaException(String msg) {
+		super(msg);
+	}
+
+}
